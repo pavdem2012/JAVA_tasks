f8f31254237c7547db030753cbc6e415e0e7aa60
pavdem2012 <pavdem2012@gmail.com>
2021-02-20 14:56:16 +1100

    add line length output

---
+++ src/com/pavdem2012/TextGames/TextGames.java
@@ -0,0 +1,17 @@
+package com.pavdem2012.TextGames;
+
+public class TextGames {
+
+    public static void main(String[] args) {
+        String Str1 = new String("Паша любит рыбалку");
+        String Str2 = new String("Обожаю придумывать настойки");
+
+        System.out.print("Длина строки \"Паша любит рыбалку\" - " );
+        System.out.println(Str1.length());
+
+        System.out.print("Длина строки \"Обожаю придумывать настойки\" - " );
+        System.out.println(Str2.length());
+
+    }
+
+}
