869b0a18af164b9a0db9caa218294a42c3df7eda
pavdem2012 <pavdem2012@gmail.com>
2021-02-20 21:42:06 +1100

    add owner metods getStringWithLength, getStringWithUpperCase, getStringWithLowerCase

--- src/com/pavdem2012/TextGames/TextGames.java
+++ src/com/pavdem2012/TextGames/TextGames.java
@@ -3,35 +3,55 @@ package com.pavdem2012.TextGames;
 public class TextGames {
 
     public static void main(String[] args) {
-        /**
-         * Вывести строки "Паша любит рыбалку", "Обожаю придумывать настойки" и их длину.
-         */
-        String text = new String("Паша любит рыбалку");
-        String text1 = new String("Обожаю придумывать настойки");
+    
+    /**
+     * Вывести строки "Паша любит рыбалку", "Обожаю придумывать настойки" и их длину.
+     */
+    String text1 = "\"Паша любит рыбалку\"";
+    String text2 = "\"Обожаю придумывать настойки\"";
+    TextGames textGames = new TextGames();
+    System.out.println(textGames.getStringWithLength(text1));
+    System.out.println(textGames.getStringWithLength(text2));
+    
+    /**
+     * Вывести строку в верхнем регистре, вывести первоначальный вариант. "Перестань кричать"
+     */
+    String text3 = "\"Перестань кричать\"";
+    System.out.println(textGames.getStringWithUpperCase(text3));
 
-        System.out.print("Длина строки \"Паша любит рыбалку\" - " );
-        System.out.println(text.length());
-
-        System.out.print("Длина строки \"Обожаю придумывать настойки\" - " );
-        System.out.println(text1 .length());
-        
-        /**
-         * Вывести строку в верхнем регистре, вывести первоначальный вариант. "Перестань кричать"
-         */
-        String text2 = new String("Перестань кричать");
-        System.out.print("В верхнем регистре: ");
-        System.out.println(text2.toUpperCase());
-        System.out.print("Первоначальный вариант: ");
-        System.out.println(text2);
-
-        /**
-         * Вывести строку в нижнем регистре, вывести первоначальный вариант. "ТИХО СКАЗАЛ"
-         */
-        String text3 = new String("ТИХО СКАЗАЛ");
-        System.out.print("В нижнем регистре: ");
-        System.out.println(text3.toLowerCase());
-        System.out.print("Первоначальный вариант: ");
-        System.out.println(text3);
+    /**
+     * Вывести строку в нижнем регистре, вывести первоначальный вариант. "ТИХО СКАЗАЛ"
+     */
+    String text4 = "\"ТИХО СКАЗАЛ\"";
+    System.out.println(textGames.getStringWithLowerCase(text4));
     }
-
+    
+    /**
+     * Вывести строки "Паша любит рыбалку", "Обожаю придумывать настойки" и их длину.
+     * @param text1 this is text for processing
+     * @param text2 this is text for processing
+     * @return String with a text length information
+     */
+    public String getStringWithLength(String inputText) {
+        String allText = inputText + ". " + "Длина текста: " + inputText.length() + ".\n";
+        return allText;
+    }
+    /**
+     * Вывести строку в верхнем регистре, вывести первоначальный вариант. "Перестань кричать"
+     * @param text3 this is text for processing
+     * @return String with a text in upper case and initial version of text4
+     */
+    public String getStringWithUpperCase(String inputText) {
+        String allText = "В верхнем регистре: " + inputText.toUpperCase() + ".\nПервоначальный вариант: " + inputText + ".\n";
+        return allText;
+    }
+    /**
+     * Вывести строку в нижнем регистре, вывести первоначальный вариант. "ТИХО СКАЗАЛ"
+     * @param text4 this is text for processing
+     * @return String with a text in lower case and initial version of text4
+     */
+    public String getStringWithLowerCase(String inputText) {
+        String allText = "В нижнем регистре: " + inputText.toLowerCase() + ".\nПервоначальный вариант: " + inputText + ".\n";
+        return allText;
+    }
 }
