07b6b692fc9290ad42dd898da0a63037cfdc7c8b
Pavdem2012 <pavdem2012@gmail.com>
2021-04-15 22:44:00 +0700

    add new package array_tasks and owner methods indicesFirstOccurrenceMaximalElement; snowflakeOutput; fillingFromMainDiagonal

---
+++ src/com/pavdem2012/textgame/array_tasks/ArrayTasks.java
@@ -0,0 +1,76 @@
+package com.pavdem2012.textgame.array_tasks;
+
+public class ArrayTasks {
+
+    /**
+     * Найдите индексы первого вхождения максимального элемента двумерного массива.
+     * 
+     * @param numberArray array for processing
+     * @return the indices of the first occurrence of an element in a
+     *         two-dimensional array.
+     */
+    public String indicesFirstOccurrenceMaximalElement(int[][] numberArray) {
+        String indicesFirstOccurrenceMaximalElement = "";
+        int cashI = 0;
+        int cashJ = 0;
+        int max = numberArray[0][0];
+        for (int i = 0; i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length; j++) {
+                if (numberArray[i][j] > max) {
+                    max = numberArray[i][j];
+                    cashI = i;
+                    cashJ = j;
+                    indicesFirstOccurrenceMaximalElement = "" + cashI + "; " + cashJ + ";";
+                }
+            }
+        }
+        System.out.println("Максимальный элемент массива: " + max + ";\n");
+        return indicesFirstOccurrenceMaximalElement;
+    }
+
+    /**
+     * Дано нечетное число n, не превосходящее 15. Создайте двумерный массив из n×n
+     * элементов, заполнив его символами "." (каждый элемент массива является
+     * строкой из одного символа). Затем заполните символами "*" среднюю строку
+     * массива, средний столбец массива, главную диагональ и побочную диагональ. В
+     * результате "*" в массиве должны образовывать изображение звездочки. Выведите
+     * полученный массив на экран, разделяя элементы массива пробелами.
+     * 
+     * @param charArray array for processing
+     */
+    public void snowflakeOutput(String[][] charArray) {
+        int n = 15;
+        System.out.println("\nМассив n x n заполненный (.):\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                charArray[i][j] = ".";
+                System.out.print(charArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nМассив со снежинкой:\n");
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < charArray[i].length; j++) {
+                if (j == n / 2 || i == n / 2 || i == j || i == n - 1 - j) {
+                    charArray[i][j] = "*";
+                }
+                System.out.print(charArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void fillingFromMainDiagonal(int[][] numberArray) {
+        System.out.println("\nЗаполненный массив:\n");
+        for (int i = 0; i < numberArray.length; i++) {
+            for (int j = 0; j < numberArray[i].length; j++) {
+                numberArray[i][j] = (int) Math.abs(i - j);
+                if (i == j) {
+                    numberArray[i][j] = 0;
+                }
+                System.out.print(numberArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/com/pavdem2012/textgame/cycles_operator/CyclesOperators.java
+++ src/com/pavdem2012/textgame/cycles_operator/CyclesOperators.java
@@ -296,11 +296,12 @@ public class CyclesOperators {
         int number = 0;
         for (int i = 0; i < numberArray.length; i++) {
             number = numberArray[i];
+            
             if (number > 0) {
                 cash = number;
             }
             for (int j = 0; j < numberArray.length; j++)
-                if (cash >= numberArray[j] & numberArray[j] > 0) {
+                if (cash >= numberArray[i] & numberArray[j] > 0) {
                     min = numberArray[j];
                     cash = min;
                 }
@@ -456,61 +457,4 @@ public class CyclesOperators {
         return uniqueArrayElements;
     }
 
-    /**
-     * Найдите индексы первого вхождения максимального элемента двумерного массива.
-     * 
-     * @param numberArray array for processing
-     * @return the indices of the first occurrence of an element in a
-     *         two-dimensional array.
-     */
-    public String indicesFirstOccurrenceMaximalElement(int[][] numberArray) {
-        String indicesFirstOccurrenceMaximalElement = "";
-        int cashI = 0;
-        int cashJ = 0;
-        int max = numberArray[0][0];
-        for (int i = 0; i < numberArray.length; i++) {
-            for (int j = 0; j < numberArray[i].length; j++) {
-                if (numberArray[i][j] > max) {
-                    max = numberArray[i][j];
-                    cashI = i;
-                    cashJ = j;
-                    indicesFirstOccurrenceMaximalElement = "" + cashI + "; " + cashJ + ";";
-                }
-            }
-        }
-        System.out.println("Максимальный элемент массива: " + max + ";\n");
-        return indicesFirstOccurrenceMaximalElement;
-    }
-
-    /**
-     * Дано нечетное число n, не превосходящее 15. Создайте двумерный массив из n×n
-     * элементов, заполнив его символами "." (каждый элемент массива является
-     * строкой из одного символа). Затем заполните символами "*" среднюю строку
-     * массива, средний столбец массива, главную диагональ и побочную диагональ. В
-     * результате "*" в массиве должны образовывать изображение звездочки. Выведите
-     * полученный массив на экран, разделяя элементы массива пробелами. 
-     * 
-     * @param Array array for processing
-     */
-    public void snowflakeOutput(String[][] Array) {
-        int n = 15;
-        System.out.println("\nМассив n x n заполненный (.):\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                Array[i][j] = ".";
-                System.out.print(Array[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("\nМассив со снежинкой:\n");
-        for (int i = 0; i < Array.length; i++) {
-            for (int j = 0; j < Array[i].length; j++) {
-                if (j == n / 2 || i == n / 2 || i == j || i == n - 1 - j) {
-                    Array[i][j] = "*";
-                }
-                System.out.print(Array[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 }
