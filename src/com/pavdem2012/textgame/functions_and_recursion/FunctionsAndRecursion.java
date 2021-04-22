65901541bccca7b8f8301d88f46728cd2537349a
Pavdem2012 <pavdem2012@gmail.com>
2021-04-22 10:58:46 +0700

    added documenting comments

--- src/com/pavdem2012/textgame/functions_and_recursion/FunctionsAndRecursion.java
+++ src/com/pavdem2012/textgame/functions_and_recursion/FunctionsAndRecursion.java
@@ -2,6 +2,12 @@ package com.pavdem2012.textgame.functions_and_recursion;
 
 public class FunctionsAndRecursion {
 
+    /**
+     * 
+     * @param a any entered number.
+     * @param b any entered number.
+     * @return minimum of any two entered numbers.
+     */
     public int getMin(int a, int b) {
         int s;
         if (a < b) {
@@ -12,6 +18,14 @@ public class FunctionsAndRecursion {
         return s;
     }
 
+    /**
+     * 
+     * @param a any entered number.
+     * @param b any entered number.
+     * @param c any entered number.
+     * @param d any entered number.
+     * @return minimum of any four entered numbers.
+     */
     public int getMin(int a, int b, int c, int d) {
         int s = getMin(a, b);
         int x;
