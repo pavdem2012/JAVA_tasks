4120568a4c23a1dce3c7b4a28d1599b1d13dc8cd
Pavdem2012 <pavdem2012@gmail.com>
2021-04-27 11:30:26 +0700

    add owner method palindromeCheck

--- src/com/pavdem2012/textgame/functions_and_recursion/FunctionsAndRecursion.java
+++ src/com/pavdem2012/textgame/functions_and_recursion/FunctionsAndRecursion.java
@@ -280,4 +280,26 @@ public class FunctionsAndRecursion {
         }
         return numbers;
     }
+
+    /**
+     * Дано слово из строчных латинских букв. Проверить, является ли оно
+     * палиндромом.
+     * 
+     * @param input any word.
+     * @return is the word palindrome or not.
+     */
+    public String palindromeCheck(String input) {
+        if (input.length() == 1) {
+            return " является палиндромом.";
+        } else {
+            if (input.substring(0, 1).equals(input.substring(input.length() - 1, input.length()))) {
+                if (input.length() == 2) {
+                    return " является палиндромом.";
+                }
+                return palindromeCheck(input.substring(1, input.length() - 1));
+            } else {
+                return " не является палиндромом.";
+            }
+        }
+    }
 }
